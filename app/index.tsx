f17038e355508f3d6cd7ee243ea9be275ef268fc
mixro <josephchongola43@gmail.com>
2025-10-12 18:10:28 +0300

    configure nativewind & tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import "../global.css"
 
 export default function Index() {
   return (
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-xl text-blue-500">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
