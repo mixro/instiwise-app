eb2b33d727d804cc1e2f7505427c038a3ff9d014
mixro <josephchongola43@gmail.com>
2025-10-21 21:07:27 +0300

    refactor: implement shared AuthBar component

--- app/Signup.tsx
+++ app/Signup.tsx
@@ -4,6 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { useTheme } from '@/src/context/ThemeContext';
 import { AntDesign, Entypo, FontAwesome, MaterialIcons } from '@expo/vector-icons';
 import { Link } from 'expo-router';
+import Authbar from '@/src/components/navigation/authbar';
 const { height } = Dimensions.get('window');
 
 export default function Signup() {
@@ -11,7 +12,6 @@ export default function Signup() {
   const [email, setEmail] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [verifiedPassword, setVerifiedPassword] = useState("");
-  const page = "signup";
 
   return (
     <SafeAreaView edges={['top', 'bottom']} style={{ flex: 1, backgroundColor: theme.background }}>
@@ -24,41 +24,7 @@ export default function Signup() {
           contentContainerStyle={styles.scrollContent}
           showsVerticalScrollIndicator={false}
         >
-          <View style={{ backgroundColor: theme.background }}>
-            <View className='flex-row items-center gap-2 flex-1 justify-center' style={{ paddingTop: 50, paddingBottom: 35 }}>
-              <Image 
-                source={require('@/assets/images/instiwise-icon.png')}
-                style={styles.logo}
-              />
-              <Text className='font-bold' 
-                style={[{ color: theme.text }, { fontSize: 20 }]}
-              >
-                INSTiWISE
-              </Text>
-            </View>
-      
-            <View className='flex-column items-center'>
-              <Text className='font-semibold mb-3' style={{ fontSize: 26, color: theme.text }}>Welcome to Instiwise</Text>
-              <Text className='text-center' style={{ fontSize: 14.5, color: theme.text }}>Sign up below to manage your{"\n"}account and access all news, events, etc</Text>
-            </View>
-      
-            <View className='flex-row justify-between' style={{ paddingTop: 45 }}>
-              <View style={{ width: "50%" }}>
-                <Link href="/login" asChild>
-                  <TouchableOpacity className='flex-row items-center justify-center' style={styles.button}>
-                    <Text className='text-lg font-bold mb-2' style={{ color: theme.icons }}>Login</Text>
-                  </TouchableOpacity>
-                </Link>
-              </View>
-              <View style={{ width: "50%" }}>
-                <Link href="/signup" asChild>
-                  <TouchableOpacity className='flex-row items-center justify-center' style={styles.active_button}>
-                    <Text className='text-lg font-bold mb-2' style={{ color: theme.icons }}>Sign Up</Text>
-                  </TouchableOpacity>
-                </Link>
-              </View>
-            </View>
-          </View>
+          <Authbar page="signup" />
 
           <View className='px-4 pt-9 pb-5' style={{ backgroundColor: "#ccccccff" }}>
             <View>
---
+++ src/components/navigation/authbar.tsx
@@ -0,0 +1,65 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { useTheme } from '@/src/context/ThemeContext';
+import { Link } from 'expo-router';
+import { StyleSheet } from 'react-native';
+
+export default function Authbar({page} : {page: string}) {
+  const { theme } = useTheme();  
+
+  return (
+    <View style={{ backgroundColor: theme.background, minHeight: "30%" }}>
+        <View className='flex-row items-center gap-2 flex-1 justify-center' style={{ paddingTop: 50, paddingBottom: 35 }}>
+            <Image 
+                source={require('@/assets/images/instiwise-icon.png')}
+                style={styles.logo}
+            />
+            <Text className='font-bold' 
+                style={[{ color: theme.text }, { fontSize: 20 }]}
+            >
+                INSTiWISE
+            </Text>
+        </View>
+    
+        <View className='flex-column items-center'>
+            <Text className='font-semibold mb-3' style={{ fontSize: 26, color: theme.text }}>Welcome to Instiwise</Text>
+            <Text className='text-center' style={{ fontSize: 14.5, color: theme.text }}>Sign up or log in below to manage your{"\n"}account and access all news, events, etc</Text>
+        </View>
+    
+        <View className='flex-row justify-between' style={{ paddingTop: 45 }}>
+            <View style={{ width: "50%" }}>
+            <Link href="/login" asChild>
+                <TouchableOpacity className='flex-row items-center justify-center' style={page === "login" ? styles.active_button : styles.button}>
+                <Text className='text-lg font-bold mb-2' style={{ color: theme.icons }}>Login</Text>
+                </TouchableOpacity>
+            </Link>
+            </View>
+            <View style={{ width: "50%" }}>
+            <Link href="/signup" asChild>
+                <TouchableOpacity className='flex-row items-center justify-center' style={page === "signup" ? styles.active_button : styles.button}>
+                <Text className='text-lg font-bold mb-2' style={{ color: theme.icons }}>Sign Up</Text>
+                </TouchableOpacity>
+            </Link>
+            </View>
+        </View>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    logo: {
+        width: 45,
+        height: 45,
+    },
+    button: {
+        width: "100%",
+        borderBottomWidth: 2.5,
+        borderBottomColor: "transparent",
+    },
+    active_button: {
+        width: "100%",
+        borderBottomWidth: 2.5,
+        borderBottomColor: "#126865ff",
+    },
+})
